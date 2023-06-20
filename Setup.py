4e8450b00d4586fda1d79f5c9897e5aff8e2c69d
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-20 14:22:08 -0400

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name=‘FHAI’,
+    version='0.0.1',
+    packages=find_packages(),
+    url=‘https://github.com/CalebFikes/FHAI’,
+    license='MIT',
+    author=‘CalebFikes’,
+    author_email=‘cjf6@rice.edu’,
+    description='',
+    install_requires=['torch', 'torchvision', 'hessQuik>=0.0.2', 'matplotlib', 'numpy', 'scikit-learn']
+)
