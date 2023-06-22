236fbff32ae50ae6fc963fa237215ca9a659d9bc
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-22 15:13:14 -0400

    Update Experiment_6-22.py

--- Conditional_MNIST/Experiment_6-22.py
+++ Conditional_MNIST/Experiment_6-22.py
@@ -1,7 +1,7 @@
 import os
 
-os.system(cd /local/scratch/cfikes/FHAI_3/Conditional_MNIST)
-os.system(pip install --no-index -r requirements.txt)
+os.system("cd /local/scratch/cfikes/FHAI_3/Conditional_MNIST")
+os.system("pip install --no-index -r requirements.txt")
 
 import copy
 import logging
