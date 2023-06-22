3f2c482fe1054006145e0e665b7494b9b5726337
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-22 15:38:57 -0400

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -3,9 +3,15 @@
 #SBATCH --output=out
 #SBATCH --gres=gpu:1
 
+# Create and activate the virtual environment
+python3.8 -m venv myenv
+source myenv/bin/activate
+
+# Install required packages
+pip install copy logging random math matplotlib==3.4.2 numpy==1.19.5 pandas==1.3.0 seaborn==0.11.1 scikit-learn==0.24.2 torch==1.8.1 torchvision==0.9.1 tqdm==4.61.1 torch-utils-data==0.2.0 imbalanced-learn==0.8.0
+
+# Change to the project directory
 cd /local/scratch/cfikes/FHAI_3/Conditional_MNIST
-python3 get-pip.py
-pip install copy, logging, random, math, matplotlib==3.4.2, numpy==1.19.5, pandas==1.3.0
-pip install seaborn==0.11.1, scikit-learn==0.24.2, torch==1.8.1, torchvision==0.9.1
-pip install , tqdm==4.61.1, torch-utils-data==0.2.0, imbalanced-learn==0.8.0
+
+# Run the Python script
 python3.8 Experiment_6-22.py
