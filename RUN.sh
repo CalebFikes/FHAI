ea06f3ac8f2462e2252104dfbef6559c94df8adb
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-22 15:06:45 -0400

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=FHAI
-#SBATCH --output=out_slurmtest
+#SBATCH --output=out
 #SBATCH --gres=gpu:1
 
-pip install --no-index -r requirements.txt
 cd /local/scratch/cfikes/FHAI_3/Conditional_MNIST
+pip install --no-index -r requirements.txt
 python3.8 Experiment_6-22.py
