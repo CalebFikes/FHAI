c480f125a102eb95f4702e53581deba5c111aa8a
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-22 15:03:44 -0400

    added requirements

--- RUN.sh
+++ RUN.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=FHAI
-#SBATCH --output=/path/to/output/out_slurmtest
+#SBATCH --output=out_slurmtest
 #SBATCH --gres=gpu:1
 
-# Load any necessary modules
-module load python3
-
-# Set the absolute path to the Python interpreter
-PYTHON_INTERPRETER= python3.8
-
-# Set the absolute path to the project directory
-PROJECT_DIR=/local/scratch/cfikes/FHAI_3/Conditional_MNIST
-
-# Install pipreqs
-$PYTHON_INTERPRETER -m pip install pipreqs
-
-# Generate requirements.txt
-cd $PROJECT_DIR
-$PYTHON_INTERPRETER -m pipreqs --force .
-
-# Install required packages
-$PYTHON_INTERPRETER -m pip install --no-index -r requirements.txt
-
-cat requirements.txt
-# Run the Python script
-$PYTHON_INTERPRETER $PROJECT_DIR/Experiment_6-22.py
+pip install --no-index -r requirements.txt
+cd /local/scratch/cfikes/FHAI_3/Conditional_MNIST
+python3.8 Experiment_6-22.py
