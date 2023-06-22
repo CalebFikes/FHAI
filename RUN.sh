d5e51fa8cf55fa04bc0b7da618e991d080930510
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-22 14:51:13 -0400

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -3,6 +3,8 @@
 #SBATCH --output=out_slurmtest
 #SBATCH --gres=gpu:1
 
-
+pip install pipreqs
+pipreqs --force
+pip install --no-index -r requirements.txt
 cd /local/scratch/cfikes/FHAI_3/Conditional_MNIST
 python3.8 Experiment_6-22.py
