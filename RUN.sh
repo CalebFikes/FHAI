00c6e0e6b39fbdf90a08635e9ac34f6b903999a5
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-22 15:16:36 -0400

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -4,6 +4,7 @@
 #SBATCH --gres=gpu:1
 
 cd /local/scratch/cfikes/FHAI_3/Conditional_MNIST
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 pip install copy, logging, random, math, matplotlib==3.4.2, numpy==1.19.5, pandas==1.3.0
 pip install seaborn==0.11.1, scikit-learn==0.24.2, torch==1.8.1, torchvision==0.9.1
 pip install , tqdm==4.61.1, torch-utils-data==0.2.0, imbalanced-learn==0.8.0
