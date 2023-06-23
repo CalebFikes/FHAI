905f1c968858e5a850a564dbd402438107eb9451
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-23 00:26:57 -0400

    Update Experiment_6-22.py

--- Conditional_MNIST/Experiment_6-22.py
+++ Conditional_MNIST/Experiment_6-22.py
@@ -495,6 +495,6 @@ def Aug_SMOTE(train):
     return X_tensor, y_tensor
 
 new_x = Aug(train, .5, n_epoch = 1)
-new_y = Aug_SMOTE(train)
+#new_y = Aug_SMOTE(train)
 print(new_x.shape)
-print(new_y.shape)
+#print(new_y.shape)
