fb6015dac54f0cd0e32cfb4d72d8137a570dee7e
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-26 15:56:40 -0400

    Update MNIST_AUG.py

--- Conditional_MNIST/MNIST_AUG.py
+++ Conditional_MNIST/MNIST_AUG.py
@@ -37,7 +37,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # configs
 configs = {
-'n_epochs' : 10, 
+'n_epochs' : 30, 
 'batch_size_train' : 128, 
 'batch_size_test' : 500, 
 'learning_rate' : 0.01, 
@@ -645,4 +645,4 @@ for trial in range(1):
 
 #os.system(tensorboard --logdir==runs)
 end_time = time.time()
-print("Time Elapsed: ", start_time - end_time)
+print("Time Elapsed: ", end_time - start_time)
