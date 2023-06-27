23e2491bac7a72b0700b80f082199c700425f36d
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-27 15:43:55 -0400

    Update MNIST_AUG.py

--- Conditional_MNIST/MNIST_AUG.py
+++ Conditional_MNIST/MNIST_AUG.py
@@ -37,11 +37,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # configs
 configs = {
-'n_epochs' : 60, 
-'batch_size_train' : 1024, 
+'n_epochs' : 45, 
+'batch_size_train' : 128, 
 'batch_size_test' : 500, 
-'learning_rate' : 0.1, 
-'momentum' : 0.2, 
+'learning_rate' : 0.01, 
+'momentum' : 0.5, 
 'log_interval' : 10,
 'class_labels' : np.array([2,7]),
 'w' : .5,
