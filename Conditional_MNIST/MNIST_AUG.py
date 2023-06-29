6623817330cc34bdc45f20327147a13e459e5f37
CalebFikes <106634531+CalebFikes@users.noreply.github.com>
2023-06-29 13:08:56 -0400

    Update MNIST_AUG.py

--- Conditional_MNIST/MNIST_AUG.py
+++ Conditional_MNIST/MNIST_AUG.py
@@ -885,7 +885,7 @@ def Aug_SMOTE(train):
 #train, test = unbalance_data(train,test,class0=3,class1=7,prop_keep=.5)
 
 # Modify the data
-data_preparer = PrepareData(train, test, .05)
+data_preparer = PrepareData(train, test, .1)
 train.data = data_preparer.train_data
 train.targets = data_preparer.train_targets
 test.data = data_preparer.test_data
@@ -895,13 +895,13 @@ test.targets = data_preparer.test_targets
 
 end_time = time.time()
 print("Time Elapsed: ", end_time - start_time)
-augment = Aug(train, .05, configs_DDPM) #treatment2
-#synth = Full_Synth(train,len(train.targets),configs_DDPM) #treatment4
+#augment = Aug(train, .1, configs_DDPM) #treatment2
+synth = Full_Synth(train,20000,configs_DDPM) #treatment4
 
 end_time = time.time()
 print("Time Elapsed: ", end_time - start_time)
-train_classifier(augment,test,configs)
-print("ABOVE IS AUG")
+train_classifier(synth,test,configs)
+print("ABOVE IS SYNTH")
 
 # end_time = time.time()
 # print("Time Elapsed: ", end_time - start_time)
